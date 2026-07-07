2f2496a1426db4d2be6ab057761a52c6ede805cb
Jota-leals <jleals.1145@gmail.com>
2026-07-06 22:26:44 -0400

    add config module

---
+++ src/config/countries.py
@@ -0,0 +1,13 @@
+COUNTRIES = {
+    "CL": "Chile",
+    'BRA': "Brazil",
+    'MEX': "Mexico",
+    'COL': "Colombia",
+    'PER': "Peru",
+    'ARG': "Argentina",
+    'USA': "United States",
+    'EZ': "Eurozone",
+    'CHN': "China",
+    'KR': "South Korea",
+    'JP': "Japan",
+}
