94575880754a4d7ba0e73bf4ab68185c5bcd60a2
Jota-leals <jleals.1145@gmail.com>
2026-07-06 22:49:53 -0400

    define base archiqueture for data sources

---
+++ src/data/fred.py
@@ -0,0 +1,6 @@
+from .base import datasource
+
+class fred(datasource):
+    def download_data(self, *args, **kwargs):
+        # Implement the logic to download data from FRED
+        raise NotImplementedError
